edcd1eb33b18e66024765b48253ae363c37ec7fa
JulissaJ <julissa.jansen@gmail.com>
2014-03-28 15:16:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogLover::Application.config.secret_key_base = '68857e77482cede04b88082bef152f8ab825e9902820ed201c161806c533bf5d0b8149b88028abffc2e257faf688445d2b72065c31c08bc3da36192b8303a23a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DogLover::Application.config.session_store :cookie_store, key: '_dog_lover_session'

b8c1f1d53d64d20eaf830a502012962a8e002291
JulissaJ <julissa.jansen@gmail.com>
2014-03-28 16:23:17 -0400

    updated schema, add contraints to migration

--- db/migrate/20140328201450_create_dog_owners.rb
+++ db/migrate/20140328201450_create_dog_owners.rb
@@ -1,10 +1,10 @@
 class CreateDogOwners < ActiveRecord::Migration
   def change
     create_table :dog_owners do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :dog_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :dog_name, null: false
 
       t.timestamps
     end
